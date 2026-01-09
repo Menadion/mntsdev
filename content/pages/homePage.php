e8a507745707fbfd6bb53f644e5114c5ada66a8c
kai <kaijambro@gmail.com>
2026-01-10 02:00:35 +0800

    page header reusable

--- content/pages/homePage.php
+++ content/pages/homePage.php
@@ -4,9 +4,11 @@
     <body class="m-0 min-vh-100">
         <div class="d-flex min-vh-100">
             <?php include "../fragments/sidebar.php"  ?>
-
             <div class="flex-grow-1 d-flex flex-column">
-                <?php include "../fragments/header.php" ?>
+                <?php
+                    $pageHeader = 'Welcome, User';
+                    include "../fragments/header.php" 
+                ?>
                 <?php include "../fragments/table.php" ?>
             </div>
         </div>
