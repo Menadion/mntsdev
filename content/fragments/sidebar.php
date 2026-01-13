6db40abc63e63570619c112318044470c659fa38
kai <kaijambro@gmail.com>
2026-01-14 01:10:24 +0800

    start of profile page and sidebar animations

--- content/fragments/sidebar.php
+++ content/fragments/sidebar.php
@@ -1,10 +1,12 @@
 <div class="sidebar d-flex flex-column align-items-center pt-3">
-    <a href="<?= ROOT_URL ?>content/pages/profilePage.php" class="sidebar-link text-decoration-none mb-1 text-center">
-        <img src="<?= ROOT_URL ?>/assets/profilepic.png" class="sb-prof-icon" alt="profile">
+    <a href="<?= ROOT_URL ?>content/pages/profilePage.php" class="sidebar-link <?= $currentPage == 'profilePage.php' ? 'active' : '' ?> text-decoration-none mb-1 text-center">
+        <div class="sb-btn">
+            <img src="<?= ROOT_URL ?>/assets/profilepic.png" class="sb-prof-icon" alt="profile">
+        </div>
         <div class="sidebar-label text-light mt-1">Profile</div>
     </a>
 
-    <a href="<?= ROOT_URL ?>content/pages/homePage.php" class="sidebar-link text-decoration-none mb-1 text-center">
+    <a href="<?= ROOT_URL ?>content/pages/homePage.php" class="sidebar-link <?= $currentPage == 'homePage.php' ? 'active' : '' ?> text-decoration-none mb-1 text-center">
         <div class="sb-btn">
             <div class="sb-icon-cont">
                 <img src="<?= ROOT_URL ?>/assets/whiteHome.svg" class="sb-icon sb-white">
--- content/head.php
+++ content/head.php
@@ -1,6 +1,7 @@
 <?php 
     define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '/mntsdev/');
     define('ROOT_URL', '/mntsdev/');
+    $currentPage = basename($_SERVER['PHP_SELF']);
 ?>
 
 <head>
